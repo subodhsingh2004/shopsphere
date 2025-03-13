ad3ef917d648518f6879ac61a3a53a8da19c19dd
Subodh Singh <singhsubodh8750@gmail.com>
2025-03-13 20:09:17 +0530

    changed apis

--- frontend/src/services/cartApi.js
+++ frontend/src/services/cartApi.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000/api/v1/cart'
+    baseURL: '/api/v1/cart'
 })
 
 export const getProductsOfCart = async (userId) => {
--- frontend/src/services/reviewApi.js
+++ frontend/src/services/reviewApi.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000/api/v1/review'
+    baseURL: '/api/v1/review'
 })
 
 export const getReviews = async (productId) => {
